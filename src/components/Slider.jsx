5e26b2f25312e377f2ced044681429b8fab312ba
Simon Jentsch <simon@bojagi.io>
2021-01-05 20:58:12 +0100

    Change background color to pink

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,8 +9,8 @@ const Bar = styled.div`
   height: 6px; 
   width: 100%;
   border-radius: 4px;
-  background-color: #eeeeee;
-  border: 1px solid #ddd;
+  background-color: #af1e66;
+  border: 1px solid #af1e66;
   position: relative;
 `;
 
